fd295f7eedcfa74c37c8e5795da73950bc0d9e57
eniola <opakunleeniolail2019@gmail.com>
2023-01-16 12:14:31 +0100

    Added shadow to cards in project.js and Added picture to home.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import linkImage from '../images/link.png'
 
 function Card(props) {
   return (
-    <div className='col-lg-3 rounded m-2 p-3 shadow' id='card-div' style={{background : props.color, height: '100px'}}>
-      <div className='card-body'>
-        <div>
+    <div className='col-lg-3 rounded m-2 p-3 card-div ' id='card-div' style={{background : props.color, height: '100px', boxShadow: `0 2px 5px 0 ${props.color}`}}>
+      <div className='card-body d-flex'>
+        <div className=''>
           <p className='card-text'>{props.title}</p>
+          <a href="" className='text-dark card-link'>Visit Site</a>
         </div>
-        <a href=""><img src="" alt="" /></a>
+        <a href="" className='' id='cardHref'><img src = {linkImage} alt=""  style= {{width: '15px'}} id='cardHref'/></a>
       </div>
     </div>
 //     <div className="card col-lg-3 rounded m-2" style={{width: "15rem;"}}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import  Navbar from '../components/Navbar'
+import profilePics from '../images/A.jpg'
 
 function Home() {
   return (
     <section id='main' className=''>
        <Navbar/>
        <div className='d-lg-flex mt-2'>
-           <div className='w-50'>
-            <img src="" alt="background image" />
+           <div className='w-lg-50 profilePicture'>
+            <img src= {profilePics} alt="background image" style={{width: '200px'}}/>
            </div>
-           <div className='w-50 p-3 rounded' id='descript-block'>
+           <div className='w-lg-50 p-3 rounded' id='descript-block'>
               <p className='lead text-center'> <b>Deborah Eniola Opakunle</b></p>
               <p>Website Developer</p>
               <p>I am Fullstack developer. I love building eautiful, functional and responsive websites. I am proficient in HTML, CSS, Bootstrap, SASS, Javascript, React.js, Angular.js, Node.js , Git and Github. I am a fast learner and I love learning new things </p>
